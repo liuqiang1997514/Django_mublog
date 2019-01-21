7b4333b289fe21ab903d607051a2fc57aadbddf8
Liuqiang <liuqiang1997514@163.com>
2019-01-21 15:43:53 +0800

    第二次提交，配置apache服务器

--- myblog/settings.py
+++ myblog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '(9y)7x(2rdv)bw7xgy=#(g3hbycx3^33nd#(u__y7el4+wcfmr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,4 +127,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/dev/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+ 
+# upload folder
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
--- myblog/wsgi.py
+++ myblog/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from os.path import join,dirname,abspath 
+
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myblog.settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myblog.settings')
+os.environ.['DJANGO_SETTINGS_MODULE'] = 'myblog.settings'
 
 application = get_wsgi_application()
+
+
+
+PROJECT_DIR = dirname(dirname(abspath(__file__)))   
+sys.path.insert(0,PROJECT_DIR)
